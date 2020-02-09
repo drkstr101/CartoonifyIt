162a168f4d4de43cc5a198f5a8b31bcdbec190cc
Aaron R Miller <aaron.miller@waweb.io>
2020-02-08 17:59:15 -0800

    generated ffi bindings for ANDKCamera

---
+++ ndk_camera_sys/wrapper.h
@@ -0,0 +1,9 @@
+#include <camera/NdkCameraCaptureSession.h>
+#include <camera/NdkCameraDevice.h>
+#include <camera/NdkCameraError.h>
+#include <camera/NdkCameraManager.h>
+#include <camera/NdkCameraMetadata.h>
+#include <camera/NdkCameraMetadataTags.h>
+#include <camera/NdkCameraWindowType.h>
+#include <camera/NdkCaptureRequest.h>
+#include "../native_app/android_glue/android_native_app_glue.h"
