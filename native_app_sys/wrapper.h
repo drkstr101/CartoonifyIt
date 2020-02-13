e2caf3605878b01afe5255cf015b0961d6498add
Aaron R Miller <aaron.miller@waweb.io>
2020-02-13 15:14:30 -0800

    added ffi bindings for native app

---
+++ native_app_sys/wrapper.h
@@ -0,0 +1,11 @@
+#include <android/native_activity.h>
+#include <android/native_window_jni.h>
+#include <android/native_window.h>
+#include <camera/NdkCameraCaptureSession.h>
+#include <camera/NdkCameraDevice.h>
+#include <camera/NdkCameraError.h>
+#include <camera/NdkCameraManager.h>
+#include <camera/NdkCameraMetadata.h>
+#include <camera/NdkCameraMetadataTags.h>
+#include <camera/NdkCameraWindowType.h>
+#include <camera/NdkCaptureRequest.h>
